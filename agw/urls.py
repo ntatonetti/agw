a6958290227086c37eff136e5caa62ff15e8cd70
Nicholas Tatonetti <nick.tatonetti@gmail.com>
2014-06-17 17:53:49 -0700

    added routings for home/root path to urls

--- agw/urls.py
+++ agw/urls.py
@@ -7,8 +7,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'agw.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^', include('home.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^facebook/', include('django_facebook.urls')),
     url(r'^accounts/', include('django_facebook.auth_urls')),
+    url(r'^wagers/', include('home.urls')),
 )
